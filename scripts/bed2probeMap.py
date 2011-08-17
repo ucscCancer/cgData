5742db052cb37da3d76d5280e176c269b4d5abe3
Kyle <kellrott@viking.(none)>
2011-08-17 12:36:41 -0700

    Getting the bed12 probe mapper working again

--- scripts/bed2probeMap.py
+++ scripts/bed2probeMap.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 
 import sys
-import cgData.geneMap
-import cgData.refGene
-import cgData.bed
-import cgData.probeMap
+import CGData.GeneMap
+import CGData.RefGene
+import CGData.Bed
+import CGData.ProbeMap
 
 from getopt import getopt
 import csv
 
-opts, args = getopt( sys.argv[1:], "".join(cgData.geneMap.optionMap.keys()) )
+opts, args = getopt( sys.argv[1:], "".join(CGData.GeneMap.optionMap.keys()) )
 
 
 #hitFunc is the function that will be used to do the overlap comparision
-hitFunc = cgData.geneMap.geneOverlap
+hitFunc = CGData.GeneMap.gene_overlap
 for a, o in opts:
-    hitFunc = cgData.geneMap.optionMap[ a[1:] ]
+    hitFunc = CGData.GeneMap.optionMap[ a[1:] ]
 
 handle = open( args[0] )
-refGene = cgData.refGene.refGene( )
+refGene = CGData.RefGene.RefGene( )
 refGene.read( handle )
 
 handle.close()
 handle = open( args[1] )
-bedFile = cgData.bed.bed( )
+bedFile = CGData.Bed.Bed( )
 bedFile.read( handle )
 
-mapper = cgData.geneMap.ProbeMapper( )
+mapper = CGData.GeneMap.ProbeMapper( )
 
-pm = cgData.probeMap.probeMap()
+pm = CGData.ProbeMap.ProbeMap()
 
 for bed in bedFile:
-    out = mapper.findOverlap( bed, refGene, hitFunc )
+    out = mapper.find_overlap( bed, refGene, hitFunc )
     o = []
     
     bed.aliases = []
