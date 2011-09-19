1326bc195dac836c1a2eac83edc7034546767314
Kyle <kellrott@viking.(none)>
2011-09-19 16:43:51 -0700

    Adding in initial parser for clinicalFeature datatype

--- CGData/ClinicalFeature.py
+++ CGData/ClinicalFeature.py
@@ -5,4 +5,14 @@ import CGData
 
 class ClinicalFeature(CGData.CGObjectBase):
     def __init__(self):
-        pass
+        self.data = None
+        
+    def read(self,handle):
+        self.data = {}
+        for line in handle:
+            tmp = line.split("\t")
+            if tmp[0] not in self.data:
+                self.data[tmp[0]] = {}
+            if tmp[1] not in self.data[tmp[0]]:
+                self.data[tmp[0]][tmp[1]] = []
+            self.data[tmp[0]][tmp[1]].append(tmp[2])
