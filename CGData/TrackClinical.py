df873d47b6de68410f837b6a60745a3f0454cde8
Kyle <kellrott@viking.(none)>
2011-09-23 16:42:36 -0700

    Adding '\,' support into stateOrder strings

--- CGData/TrackClinical.py
+++ CGData/TrackClinical.py
@@ -1,6 +1,6 @@
 
 import CGData
-
+import re
 
 
 class TrackClinical(CGData.CGMergeObject,CGData.CGSQLObject):
@@ -18,26 +18,51 @@ class TrackClinical(CGData.CGMergeObject,CGData.CGSQLObject):
     
     def gen_sql(self, id_table):
         CGData.log("ClincalTrack SQL " + self.get_name())
-        
-        yield "clinical track testing\n"
-        
+                
         matrix = self.members["clinicalMatrix"]        
         matrix.feature_type_setup()        
         features = self.members["clinicalFeature"]
-        print features
+        #print features
         for a in features:
             if "stateOrder" in features[a]:
-                tmp = features[a]["stateOrder"][0].split(',')
+                #print features[a]["stateOrder"][0]
+                
+                #this weird bit of code is to split on ',', but respect \,
+                #if you can think of a better way, please replace this
+                tmp = re.split(r'([^,]),', features[a]["stateOrder"][0])
+                enums = []
+                word = True
+                appending = False
+                e = 0
+                while e < len(tmp): 
+                    if word:
+                        if appending:
+                            enums[-1] += tmp[e]
+                        else:
+                            enums.append(tmp[e])
+                        word = False
+                    else:
+                        if tmp[e] != "\\":
+                            enums[-1] += tmp[e]
+                            appending = False
+                        else:
+                            enums[-1] += ","
+                            appending = True
+                        word = True
+                    e += 1
+                
+                #print tmp
+                #print enums
+                #print matrix.enum_map[a]
                 i = 0
                 for e in matrix.enum_map[a]:
-                    if e in tmp:
-                        matrix.enum_map[a][e] = tmp.index(e)
+                    if e in enums:
+                        matrix.enum_map[a][e] = enums.index(e)
                     else:
-                        matrix.enum_map[a][e] = len(tmp) + i
+                        matrix.enum_map[a][e] = len(enums) + i
                         i += 1
-                print tmp
-                print matrix.enum_map[a]
-        
+                #print matrix.enum_map[a]
+                #print "-=-=-=-=-"
         for a in matrix.gen_sql(id_table, skip_feature_setup=True):
             yield a
     
