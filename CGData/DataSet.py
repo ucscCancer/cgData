168cd6a3d7dc420a153fd9daa6d54e0b36cc8c00
Brian Craft <craft@soe.ucsc.edu>
2011-10-21 13:54:41 -0700

    Make DataSet inherit from dict.
    instead of duplicating all the dict methods.

--- CGData/DataSet.py
+++ CGData/DataSet.py
@@ -4,11 +4,11 @@ import CGData
 from glob import glob
 import json
 
-class DataSet:
+class DataSet(dict):
     
     def __init__(self,params={}):
-        self.set_hash = {}
         self.params = params
+        super(DataSet,self).__init__()
 
     def scan_dirs(self, dirs):
         for dir in dirs:
@@ -26,9 +26,9 @@ class DataSet:
 
                         if (data is not None and 'name' in data 
                         and data['name'] is not None
-                        and 'type' in data):                
+                        and 'type' in data):
                             self.addFile(data['type'], data['name'], path)
-                    
+
                     if path.endswith("*.cgz"):
                         cgzList = CGData.CGZ.list( path )
                         for type in cgzList:
@@ -36,36 +36,28 @@ class DataSet:
                                 self.addFile(type, cgzList[type][zPath], zPath, path)
                 if os.path.isdir(path):
                     self.scan_dirs([path])
-        
+
         if "filter" in self.params:
             for t in self.params["filter"]:
-                if t in self.set_hash:
+                if t in self:
                     removeList = []
-                    for name in self.set_hash[t]:
+                    for name in self[t]:
                         if not re.search( self.params["filter"][t], name):
                             removeList.append(name)
-                    
+
                     for name in removeList:
-                        del self.set_hash[t][name]
-                        
+                        del self[t][name]
+
     def addFile(self, type, name, path, zipFile=None):
         if CGData.has_type(type):
-            if not type in self.set_hash:
-                self.set_hash[ type ] = {}
-                        
-            if name in self.set_hash[type]:
+            if not type in self:
+                self[ type ] = {}
+
+            if name in self[type]:
                 CGData.error("Duplicate %s file %s" % (
                 type, name))
-            self.set_hash[type][name] = CGData.light_load( path, zipFile )
+            self[type][name] = CGData.light_load( path, zipFile )
             CGData.log("FOUND: " + type +
                 "\t" + name + "\t" + path)
         else:
             CGData.warn("Unknown file type: %s" % (path))
-                
-
-    def __iter__(self):
-        return self.set_hash.__iter__()
-
-    def __getitem__(self, item):
-        return self.set_hash[item]
-
